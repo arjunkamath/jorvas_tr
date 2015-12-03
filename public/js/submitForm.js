034e78176907faaba42a03ebcc13bc5dc95fb820
Arjun <junnaonly@gmail.com>
2015-12-04 00:10:45 +0200

    messy but something is working

--- public/js/submitForm.js
+++ public/js/submitForm.js
@@ -1,13 +1,28 @@
-var signumForm = $('#signum').val();
+var signumForm = $('.signum').val();
 var dayForm = localStorage.getItem("todayDay");
 var entryTimeForm = localStorage.getItem("savedEntryTime");
 var exitTimeForm = localStorage.getItem("savedExitTime");
 
+$("#signum").change(function(){
+    console.log('Typing');
+});
+
 console.log(signumForm);
+console.log(document.getElementById('text').value);
 console.log(dayForm);
 console.log(entryTimeForm);
 console.log(exitTimeForm);
 
 $('#submit').click(function() {
 	$.post("/saveData", {signum : signumForm, todayDay : dayForm, entryTime : entryTimeForm, exitTime : exitTimeForm});
+});
+
+
+$('#text_value').click(function() {
+var text_value = $("#text").val();
+if(text_value=='') {
+alert("Enter Some Text In Input Field");
+}else{
+alert(text_value);
+}
 });
