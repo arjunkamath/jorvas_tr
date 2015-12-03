d1675dc4890508378a912f4cf507ad20fd341d5f
Arjun <junnaonly@gmail.com>
2015-12-04 00:26:15 +0200

    submit works also trying to add button for db

--- public/js/submitForm.js
+++ public/js/submitForm.js
@@ -1,29 +1,18 @@
-var signumForm = $('.signum').val();
 var dayForm = localStorage.getItem("todayDay");
 var entryTimeForm = localStorage.getItem("savedEntryTime");
 var exitTimeForm = localStorage.getItem("savedExitTime");
 
-$("#signum").change(function(){
-    console.log('Typing');
-});
-
-console.log(signumForm);
-console.log(document.getElementById('text').value);
-console.log(dayForm);
-console.log(entryTimeForm);
-console.log(exitTimeForm);
-
-$('#submit').click(function() {
-	$.post("/saveData", {signum : signumForm, todayDay : dayForm, entryTime : entryTimeForm, exitTime : exitTimeForm});
-});
-
-
-$('#text_value').click(function() {
-	var text_value = $("#text").val();
+$('#submitData').click(function() {
+	var text_value = $("#signumVal").val();
 	if(text_value=='') {
-		alert("Enter Some Text In Input Field");
+		alert("Please enter signum");
 	} else {
-		alert(text_value);
+		//alert(text_value);
 		$.post("/saveData", {signum : text_value, todayDay : dayForm, entryTime : entryTimeForm, exitTime : exitTimeForm});
 	}
 });
+
+$('#gotoDB').click(function() {
+	$.get("/db");
+});
+
