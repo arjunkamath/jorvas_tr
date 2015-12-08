4580b7ce1de183487acc1b722718223a5ea700d4
Arjun <junnaonly@gmail.com>
2015-12-08 21:01:24 +0200

    improved sorting when seeing database

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/db', function (request, response) {
 
 app.post('/ownDB', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("SELECT day, entry, exit FROM flexim_data WHERE signum = ($1)", [request.body.signum], function(err, result) {
+    client.query("SELECT day, entry, exit FROM flexim_data WHERE signum = ($1) order by day desc", [request.body.signum], function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
